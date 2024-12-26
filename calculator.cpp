c173f5c103255da28b349f3ffec909d19ab0534e
Sameersingh-code <sameersinghmodanwal@gmail.com>
2024-12-27 01:29:46 +0530

    c_program

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"enter a first number"<<endl;
+    cin >> a;
+     cout<<"enter a second number"<<endl;
+    cin >> b;
+    cout<<"sum = "<<(a+b)<<endl;
+    cout<<"diffence = "<<(a-b) <<endl;
+    cout<<"multiple = "<<(a*b)<<endl;
+    cout<<"division = "<<(a/b)<<endl;
+    cout<<"module = "<<(a%b)<<endl;
+
+
+
+
+
+    return 0;
+}
