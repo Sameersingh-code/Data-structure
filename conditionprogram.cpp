115ce1a8de905c34e04795e77541b27d15c5dd39
Sameer singh <Sameersinghmodanwal@gmail.com>
2025-01-02 00:29:52 +0530

    Create conditionprogram.cpp
    It's some condition program.

---
+++ conditionprogram.cpp
@@ -0,0 +1,66 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+    // How to find number is positive or negative
+    // int i;
+    // cout<<"Enter a number" <<endl;
+    // cin>>i;
+    // if(i>=0){
+    //     cout<<"your number is positive"<<endl;
+    // }else{
+    //     cout<<"your number is negative" <<endl;
+    // }
+    
+    // How to find number is even or odd
+    
+    // int i;
+    // cout<<"Enter a number"<<endl;
+    // cin>>i;
+    // if(i%2==0){
+    //     cout<<"your number is even";
+    // }else if(i%2!=0){
+    //     cout<<"your number is odd";
+    // }
+    
+    //How to calculate sum of two number
+    // int sum = 0;
+    // int n;
+    // cout<<"Enter a number"<<endl;
+    // cin>>n;
+    // for(int i = 1; i<=n; i++){
+    //     //sum = sum +1;
+    //     //sum+=sum;
+    //     sum = sum + i;
+    // }
+    // cout<<sum<<endl;
+    
+    // How to calculate mul of two number
+    // int mul = 1;
+    // int n;
+    // cout<<"Enter a number"<<endl;
+    // cin>>n;
+    // for(int i = 1; i<=n; i++){
+    //     mul = mul * i;
+    // }
+    // cout<<mul<<endl;
+    
+    // How to make grade behalf of marks
+
+    // int marks;
+    // cout<<"Enter your percentage"<<endl;
+    // cin>>marks;
+    // if(marks>=90){
+    //     cout<<"A";
+    // }else if(marks<90&&marks>=80){
+    //     cout<<"B";
+    // }else if(marks<80&&marks>=70){
+    //     cout<<"C";
+    // }else{
+    //     cout<<"D";
+    // }
+    
+    
+    
+    
+    
+// }
